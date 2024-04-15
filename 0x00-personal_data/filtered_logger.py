2333b01fdc27ac9b42a7001c9a26fe083dae5d26
THEKINGSTAR <thekingstar5002@gmail.com>
2024-04-15 20:02:48 +0200

    REPO INITIATE

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Personal data
+"""
+import re
+
+
+"""
+fields = ["password", "date_of_birth"]
+messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
+"name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
+"""
+"""
+name=egg;email=eggmin@eggsample.com;password=xxx;date_of_birth=xxx;
+"""
+
+
+def filter_datum(fields: list[str, list], redaction: str, message: str, separator: str) -> str:
+    """
+    function called filter_datum that returns the log message obfuscated:
+    Arguments:
+    fields: a list of strings representing all fields to obfuscate
+    redaction: a string representing by what the field will be obfuscated
+    message: a string representing the log line
+    separator: a string representing by which character is separating all fields in the log line (message)
+    The function should use a regex to replace occurrences of certain field values.
+    filter_datum
+    should be less than 5 lines long and use
+    re.sub to perform the substitution with a single regex.
+    """
+    pattern = '|'.join(fields)
+    obfuscated = re.sub(pattern + r'=([^{}]+)'.format(separator),
+                        r'\1={}{}'.format(redaction, separator), message)
+    
+    return obfuscated
---
+++ 0x00-personal_data/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+filter_datum = __import__('filtered_logger').filter_datum
+
+fields = ["password", "date_of_birth"]
+messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
+
+for message in messages:
+    print(filter_datum(fields, 'xxx', message, ';'))
+
+
+"""
+bob@dylan:~$
+bob@dylan:~$ ./main.py
+name=egg;email=eggmin@eggsample.com;password=xxx;date_of_birth=xxx;
+name=bob;email=bob@dylan.com;password=xxx;date_of_birth=xxx;
+bob@dylan:~$
+"""
