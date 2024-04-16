0f065cff28ed6175c8fd46a0d1013fd5327a7445
THEKINGSTAR <thekingstar5002@gmail.com>
2024-04-17 00:35:41 +0200

    correct the route for the @app_views.route(/unauthorized,

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized/',
+@app_views.route("/unauthorized",
                  methods=['GET'], strict_slashes=False)
 def get_Unauthorized() -> str:
     """
