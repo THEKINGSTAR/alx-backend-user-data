d5501fcd5680bc132939917d992fd41f3d2c92fd
THEKINGSTAR <thekingstar5002@gmail.com>
2024-04-16 23:48:27 +0200

    try abort 401 error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -3,6 +3,7 @@
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -24,3 +25,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/api/v1/unauthorized/', methods=['GET'], strict_slashes=False)
+def get_Unauthorized()-> str:
+    """
+    GET /api/v1/unauthorized
+    This endpoint must raise a 401 error by using abort
+    """
+    abort(401, description="Unauthorized")
