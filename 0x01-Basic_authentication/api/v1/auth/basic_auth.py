6681225736bd7c6b924a10605a320ecdbed395fb
THEKINGSTAR <thekingstar5002@gmail.com>
2024-04-20 06:50:07 +0200

    10. Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,9 @@ Create a class BasicAuth that inherits from Auth.
 
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
+from models.base import Base
 
 
 class BasicAuth(Auth):
@@ -77,5 +80,34 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
         else:
-            email, password = decoded_base64_authorization_header.split(':',1)
+            email, password = decoded_base64_authorization_header.split(':', 1)
             return (email, password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Return None if user_email is None or not a string
+        Return None if user_pwd is None or not a string
+        Return None if your database (file)
+            doesn’t contain any User instance
+            with email equal to user_email -
+            you should use the class method search of the User
+            to lookup the list of users based on their email.
+            Don’t forget to test all cases:
+            “what if there is no user in DB?”, etc.
+        Return None if user_pwd is not the password of the User instance found
+        - you must use the method is_valid_password of User
+        Otherwise, return the User instance
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        users = User.search({"email": user_email})
+
+        if not users:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
