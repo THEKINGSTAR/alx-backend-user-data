269b12cec4dad5ebe56151c1af46175a6a7b0f64
THEKINGSTAR <thekingstar5002@gmail.com>
2024-04-20 05:01:12 +0200

    correct the return value

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -77,4 +77,5 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
         else:
-            return decoded_base64_authorization_header
+            email, password = decoded_base64_authorization_header.split(':',1)
+            return (email, password)
