614396c827fe002a9f535e5af4c0fc30046aa7cd
THEKINGSTAR <thekingstar5002@gmail.com>
2024-04-21 04:20:23 +0200

    FIXY PEP8 STYLE

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,6 +7,9 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -17,13 +20,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 AUTH_TYPE = getenv("AUTH_TYPE", "auth")
 
 if AUTH_TYPE == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 if AUTH_TYPE == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 else:
-    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
 """
-Create the class Auth:
-in the file api/v1/auth/auth.py
+Create the class SessionAuth:
+in the file api/v1/auth/SessionAuth.py
 """
 
 
 from flask import request
 from typing import TypeVar, List
-User = TypeVar('User')
 from api.v1.auth.auth import Auth
-
+User = TypeVar('User')
 
 
 class SessionAuth(Auth):
     """
     SessionAuth that inherits from Auth
     """
-    
