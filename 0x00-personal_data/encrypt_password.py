f606ea8c2b3ace05f1898d27fc9a4f9b4458fc2b
THEKINGSTAR <thekingstar5002@gmail.com>
2024-05-17 01:56:39 +0300

    6. Check valid password (mandatory)

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -20,3 +20,18 @@ def hash_password(password: str) -> bytes:
     hash_password = bcrypt.hashpw(pass_encode, bcrypt.gensalt())
 
     return hash_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    function that expects 2 arguments and returns a boolean.
+    Arguments:
+    hashed_password: bytes type
+    password: string type
+    Use bcrypt to validate that the provided password
+    matches the hashed password.
+    """
+    pass_encode = password.encode()
+    if bcrypt.checkpw(pass_encode, hashed_password):
+        return True
+    return False
