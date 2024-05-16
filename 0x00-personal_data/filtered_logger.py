ed394b07a4c655f4f16c5cf6985c905ddf9d0f93
THEKINGSTAR <thekingstar5002@gmail.com>
2024-05-16 22:04:53 +0300

    2. Create logger (mandatory)

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,7 +18,7 @@ fields = ["name",
           "email",
           "password",
           "date_of_birth"]
-PII_FIELDS = ('name', 'email', 'phone', 'last_login', "user_agent")
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str,
