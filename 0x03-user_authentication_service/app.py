6a75cff127104bf8aa6390afa046c831a09b6d67
THEKINGSTAR <thekingstar5002@gmail.com>
2024-05-30 23:41:19 +0300

    7. Register user (mandatory)

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,7 +4,6 @@
 """
 
 
-from db import DB
 from user import User, Base
 from sqlalchemy.orm.exc import NoResultFound
 from flask import Flask, jsonify, request
@@ -26,30 +25,31 @@ def single_GET():
 
 
 @app.route('/users', methods=['POST'])
-def post_users(email: str, password: str):
+def post_users():
     """
-    Define a users function
-    that
+    a users function that
     implements the POST /users route.
-    the end-point should register it
-    and
+    the end-point should register it and
     respond with the following JSON payload:
     {"email": "<registered email>", "message": "user created"}
-    If the user is already registered, catch the exception
-    and
+    If the user is already registered,
+    catch the exception and
     return a JSON payload of the form
     {"message": "email already registered"}
     """
-    email = request.json.get('email')
-    password = request.json.get('password')
-    if not email or not password:
-        return jsonify({"message": "email and password is required"})
-
     try:
-        AUTH.register_user(email, password)
-        return jsonify({"email": "{email}", "message": "user created"})
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if not email or not password:
+            return jsonify({"message": "email and password are required"}), 400
+
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+
     except ValueError:
-        return jsonify({"message": "email already registered"})
+        return jsonify({"message": "email already registered"}), 400
+# @app.route('/users', methods=['GET'])
 
 
 if __name__ == "__main__":
